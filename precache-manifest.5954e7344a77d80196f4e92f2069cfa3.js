332ca5cb49d3988c54be0cc952c9dc4ab5eb72c0
naTpuk-ep <naTpuk.ep@gmail.com>
2020-10-26 15:34:28 +0300

    Updates

---
+++ precache-manifest.5954e7344a77d80196f4e92f2069cfa3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8749604a98832e15b379d8fc300f13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "116bab548f502306981f",
+    "url": "/static/css/main.6d6917a8.chunk.css"
+  },
+  {
+    "revision": "500da15ebe32b2a09536",
+    "url": "/static/js/2.67aa273f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.67aa273f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116bab548f502306981f",
+    "url": "/static/js/main.2242c6b4.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "dc8dc7aaaa4da39106478ec24e939fbf",
+    "url": "/static/media/CNY.dc8dc7aa.png"
+  },
+  {
+    "revision": "e1c9768d02aff22f203c4f93d0fe670d",
+    "url": "/static/media/EUR.e1c9768d.png"
+  },
+  {
+    "revision": "38c1328568118ccee45db1e5b93b5e71",
+    "url": "/static/media/GBP.38c13285.png"
+  },
+  {
+    "revision": "9987aabc6c809425e08b51935120d51a",
+    "url": "/static/media/JPY.9987aabc.png"
+  },
+  {
+    "revision": "0c79b82e54d1e0e2c1ef7f03be71a687",
+    "url": "/static/media/RUB.0c79b82e.png"
+  },
+  {
+    "revision": "ff97e132da519724034302a4a92750ef",
+    "url": "/static/media/USD.ff97e132.png"
+  }
+]);
